2aea5d86272a546ec41dcb6350a4e4065720fbb0
Rachael <rachael.ellen@btopenworld.com>
2018-03-12 21:57:59 +0000

    disastrous attempt at library homework

--- library/library.rb
+++ library/library.rb
@@ -1,39 +1,58 @@
 class Library
 
-attr_reader :books
+  attr_reader :books
 
-def initialize(book_collection)
-  @books = book_collection
+  def initialize(book_collection)
+    @books = book_collection
 
-end
+  end
 
-def check_rental_details(book_title)
-  for book in @books
-    if book[:title] == book_title
-      return book[:rental_details]
+  def check_rental_details(book_title)
+    for book in @books
+      if book[:title] == book_title
+        return book[:rental_details]
+      else
+        return
+      end
     end
   end
-end
+
+    # def add_book(book_title)
+    #     @books.push(book_title[:title])
+    #
+    # end
+
+    def add_book_to_library(book_title)
+
+      added_book = {
+        title: "",
+        rental_details: {
+          student_name: "",
+          date: ""
+        }
+      }
 
 
+      for book in @books
+        if book[:title] == book_title
+          return
+        elsif book[:title] != book_title
+          @books << added_book
+        end
+      end
+      return
+    end
 
+      # for book in @books
+      #   book_not_in_library = true if book[:title] != book_title
+      # end
+      #
+      # added_book[:title] << book_title if book_not_in_library == true
+      #
+      # @books << added_book
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # end
+    #
+    # def method_name
 
 end
--- library/specs/library_specs.rb
+++ library/specs/library_specs.rb
@@ -21,7 +21,7 @@ class TestLibrary < MiniTest::Test
 
   end
 
-  def test_check_rental_details
+  def test_check_rental_details__true
      result = @library.check_rental_details("lord_of_the_rings")
 
     assert_equal({
@@ -29,4 +29,22 @@ class TestLibrary < MiniTest::Test
       date: "01/12/16"
     }, result)
   end
+
+
+  def test_check_rental_details__false
+   result = @library.check_rental_details("peter_rabbit")
+
+  assert_equal(nil, result)
+  end
+
+  def test_add_book_to_library
+    result = @library.add_book_to_library("the_name_of_the_wind")
+    assert_equal("4", @library.books.length)
+  end
+
+  # def test_change_rental_details
+  #
+  #
+  # end
+
 end
